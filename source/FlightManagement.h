58ba30a714c56b7e36db502da5d77a8254654bda
Duarte Lagoela <up202205629@up.pt>
2023-12-20 12:33:43 +0000

    Functionalities Template

--- source/FlightManagement.h
+++ source/FlightManagement.h
@@ -41,6 +41,58 @@ public:
     void addAirport(airport airport);
     void addAirline(airline airline);
     void addFlight(string sourceCode, string targetCode, string airlineCode);
+
+    /**Functionalities*/
+
+    //Modify return type and parameters as needed
+
+    //i
+    int nAirports();
+    int nAvailableFlights();
+
+    //ii
+    int nFlightsFromAirport(); //n flights and different companies
+
+    //iii
+    int nFlightsCity();
+    int nFlightsAirline();
+
+    //iv
+    //In one flight
+    int nCountriesFromAirport();
+    int nCountriesFromCity();
+
+    //v
+
+    int reachableCountriesFromAirport();
+
+
+    //vi
+    int reachableAirportsInXStops();
+    int reachableCitiesInXStops();
+    int reachableCountriesInXStops();
+
+    //vii
+    void maxTrip(const string& airportCode);
+
+    //viii
+    void topTrafficAirports(int k);
+
+    //ix
+    void essentialAirports();//articulation points
+
+    /**Best Flight Option*/
+
+    void bestFlightAirportCode(const string& sourceCode, const string& targetCode);
+    void bestFlightAirportName(const string sourceName, const string& targetName);
+
+    void bestFlightCity(const string sourceName, const string& targetName);
+
+    void bestFlightGeographical();
+
+    //Combinations of these
+
+
 };
 
 
